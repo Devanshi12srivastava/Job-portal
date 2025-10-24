5e8cb5205b7a5e8d6ce2f896f0814dadd177f832
Devanshi12srivastava <devanshisrivastava164@gmail.com>
2025-10-24 18:30:24 +0530

    url

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 const corsOption={
     origin:['http://localhost:5173',
-    'job-portal-rho-teal.vercel.app'],
+   "https://job-portal-rho-teal.vercel.app"],
     credentials:true
 };
 app.use(cors(corsOption));
