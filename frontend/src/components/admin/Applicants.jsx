06cced80489112730988bed1bac4058796345d24
Devanshi12srivastava <devanshisrivastava164@gmail.com>
2025-11-06 15:09:32 +0530

    add

--- frontend/src/components/admin/Applicants.jsx
+++ frontend/src/components/admin/Applicants.jsx
@@ -24,7 +24,7 @@ const Applicants = () => {
         console.log("🟢 API Response:", res.data);
         if (res.data.success) {
          
-       dispatch(setAllApplicants({ applications: res?.data?.job?.applications }));
+       dispatch(setAllApplicants({ applications: res?.data?.job?.applications?.applicant }));
 
         }
       } catch (error) {
