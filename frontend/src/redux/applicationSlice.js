f04abb805753da73522c26a84c7d488faa5de3d5
Devanshi12srivastava <devanshisrivastava164@gmail.com>
2025-11-06 15:07:45 +0530

    add

--- frontend/src/redux/applicationSlice.js
+++ frontend/src/redux/applicationSlice.js
@@ -1,15 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const applicationSlice=createSlice({
-    name:'application',
-    initialState:{
-        applicants:[],
+const applicationSlice = createSlice({
+  name: "application",
+  initialState: {
+    applicants: {
+      applications: [], // ✅ default empty array (important)
     },
-    reducers:{
-        setAllApplicants:(state,action)=>{
-            state.applicants=action.payload;
-        }
-    }
+  },
+  reducers: {
+    setAllApplicants: (state, action) => {
+      state.applicants = action.payload;
+    },
+  },
 });
-export const {setAllApplicants}=applicationSlice.actions;
+
+export const { setAllApplicants } = applicationSlice.actions;
 export default applicationSlice.reducer;
